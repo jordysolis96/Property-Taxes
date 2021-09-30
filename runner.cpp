1079600b50930743a00816af931de65da5e4541c
jordy muniz <jordy.muniz96@gmail.com>
2021-09-29 23:49:28 -0500

    added account number method with length validation

--- runner.cpp
+++ runner.cpp
@@ -4,8 +4,10 @@
 
 using namespace std;
 
-int ACCOUNT_LENGTH;
+int ACCOUNT_LENGTH = 6;
+string accountNumber;
 string str;
+int input;
 
 enum Property
 {
@@ -22,7 +24,7 @@ void app()
                 for(int i=0;i<str.length();++i)
                 {
 
-                        if(isalpha(str[i]) == true)
+                        if(isalpha(str.at(i)))
                         {
                         cout<< "Welcome " + str<<endl;
                         break;
@@ -35,8 +37,29 @@ void app()
 }
 }
 
+void accountNum(){
+        cout << "Enter your account number:" << endl;
+        cin >> accountNumber;
+        if(accountNumber.size() == ACCOUNT_LENGTH){
+                cout << "How many properties do you want to process?" << endl;
+        }else if(accountNumber.size() > ACCOUNT_LENGTH){
+                cout << "Input was too big. Your Account number should be 6 characters" << endl;
+                accountNum();
+        }else if(accountNumber.size() < ACCOUNT_LENGTH){
+                cout << "Input too small. Your Account number should be 6 characters" << endl;
+                accountNum();
+        }else{
+                cout << "unexpected error. Please make sure your Account number is 6 characters" << endl;
+                accountNum();
+        }
+    
+
+}
+
+
 int main(){
         app();
+        accountNum();
         cout << "next method call goes here" << endl;
         return 0;
 }
