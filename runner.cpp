51d7ed8721f18c433926e53927e5b559e89458ee
jordy muniz <jordy.muniz96@gmail.com>
2021-09-30 21:20:26 -0500

    name and account number validity fully completed

--- runner.cpp
+++ runner.cpp
@@ -4,11 +4,12 @@
 
 using namespace std;
 
-int ACCOUNT_LENGTH = 6;
+const int ACCOUNT_LENGTH = 6;
 string accountNumber;
 string str;
 int input;
 bool alphaNum;
+bool valid;
 
 enum Property
 {
@@ -16,83 +17,59 @@ enum Property
         Residual = 2
 };
 
-void app()
-{
-
-
-        // do{
-        //         cout << "Enter your name: " << endl;
-        // getline(cin, str);
-        // }while (/* condition */)
-        // {
-        //         /* code */
-        // }
-        
-        cout << "Enter your name: " << endl;
-        getline(cin, str);
-
-        for (int i = 0; i < str.length(); ++i)
-        {
-
-                if (isalpha(str[i]))
-                {
-                        cout << "Welcome " + str << endl;
-                        break;
-                }else if(!isalpha(str[i])){
-                        cout << "This is not alphabetical" << endl;
-                }else
-                {
-                        cout << "Your name can only have alphabets or spaces. Enter again." << endl;
-                        app();
-                }
-        }
-}
-
-void accountNum()
-{
-
-              //having boolean set to false but when right conditions are met set to true and while loop checks to see if false
-       while(alphaNum == false){
-        do{
-
-        cout << "Enter your account number:" << endl;
-        cin >> accountNumber;
-        for(int i=0;i<accountNumber.length();i++){ 
-      
-        if(!isalnum(accountNumber[i])){
-                alphaNum = false;
-                cout << "can only accept alphanumric values" << endl;
-                
-                break;
-        }                
-        else if (accountNumber.size() > ACCOUNT_LENGTH)
-        {
-                alphaNum = false;
-                cout << "Your account number must have 6 alphanumeric characters. Please enter again." << endl;
-                
-               break;
-        }
-        else if (accountNumber.size() < ACCOUNT_LENGTH)
-        {
-                alphaNum = false;
-                cout << "Your account number must have 6 alphanumeric characters. Please enter again." << endl;
-  
-               break;
-        }else{
-                alphaNum = true;
-                cout << "success" << endl;
-                break;
-        }
-        }
-        }while (accountNumber.size() != ACCOUNT_LENGTH);
-       }
-        
-}
 
 int main()
 {
-        app();
-        accountNum();
+        //needs to check for spaces and set boolean to true if space or alphabetical inputs are inserted. If input what non-alphabetical then boolean equals false recursively executing the do. When boolean is true, loop will break free
+        do{
+                cout<<"Enter your name: "<<endl;
+                getline(cin,str);
+                for (int i = 0; i < str.length(); i++)
+                {
+                        if(isspace(str.at(i))){
+                                valid = true;
+                        }else if(!isalpha(str[i])){
+                        cout << "Your name can only have alphabets or spaces. Enter again." << endl;
+                        valid = false;
+                        break;
+                        }else{
+                                valid = true;
+                        }
+                }
+                
+        }while(valid == false);
+
+ 
+        //having boolean set to false but when right conditions are met set to true and while loop checks to see if false
+         do{
+        cout << "Enter your account number:" << endl;
+        cin >> accountNumber;
+        for(int i=0;i<accountNumber.length();i++){ 
+        if(!isalnum(accountNumber[i])){
+               
+                cout << "can only accept alphanumric values" << endl;
+                alphaNum = false;
+                break;
+        
+        }
+        else if (accountNumber.size() > ACCOUNT_LENGTH)
+        {
+               
+                cout << "Your account number must have 6 alphanumeric characters. Please enter again." << endl;
+                break;
+        }
+        else if (accountNumber.size() < ACCOUNT_LENGTH)
+        {
+              
+                cout << "Your account number must have 6 alphanumeric characters. Please enter again." << endl;
+
+               break;
+        }else{
+                alphaNum = true;
+        }
+        }
+
+        }while (accountNumber.size() != ACCOUNT_LENGTH || alphaNum == false);
         cout << "next method call goes here" << endl;
         return 0;
 }
