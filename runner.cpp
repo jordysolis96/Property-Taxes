2e9957e17cb6f6dd1980fc0912308a6904c2bb7d
jordy muniz <jordy.muniz96@gmail.com>
2021-09-30 19:00:51 -0500

    updated method

--- runner.cpp
+++ runner.cpp
@@ -8,59 +8,91 @@ int ACCOUNT_LENGTH = 6;
 string accountNumber;
 string str;
 int input;
+bool alphaNum;
 
 enum Property
 {
-    Commercial = 1,
-    Residual = 2
+        Commercial = 1,
+        Residual = 2
 };
 
 void app()
 {
-     cout<<"Enter your name: "<<endl;
-        getline(cin,str);
 
 
-                for(int i=0;i<str.length();++i)
+        // do{
+        //         cout << "Enter your name: " << endl;
+        // getline(cin, str);
+        // }while (/* condition */)
+        // {
+        //         /* code */
+        // }
+        
+        cout << "Enter your name: " << endl;
+        getline(cin, str);
+
+        for (int i = 0; i < str.length(); ++i)
+        {
+
+                if (isalpha(str[i]))
                 {
-
-                        if(isalpha(str.at(i)))
-                        {
-                        cout<< "Welcome " + str<<endl;
+                        cout << "Welcome " + str << endl;
                         break;
-                        }           
-                        else
-                        {
-                                cout<<"Your name can only have alphabets or spaces. Enter again."<<endl;
-                                app();
-                        }
-}
+                }else if(!isalpha(str[i])){
+                        cout << "This is not alphabetical" << endl;
+                }else
+                {
+                        cout << "Your name can only have alphabets or spaces. Enter again." << endl;
+                        app();
+                }
+        }
 }
 
-void accountNum(){
+void accountNum()
+{
+
+              //having boolean set to false but when right conditions are met set to true and while loop checks to see if false
+       while(alphaNum == false){
+        do{
+
         cout << "Enter your account number:" << endl;
         cin >> accountNumber;
-        if(accountNumber.size() == ACCOUNT_LENGTH){
-                cout << "How many properties do you want to process?" << endl;
-        }else if(accountNumber.size() > ACCOUNT_LENGTH){
-                cout << "Input was too big. Your Account number should be 6 characters" << endl;
-                accountNum();
-        }else if(accountNumber.size() < ACCOUNT_LENGTH){
-                cout << "Input too small. Your Account number should be 6 characters" << endl;
-                accountNum();
-        }else{
-                cout << "unexpected error. Please make sure your Account number is 6 characters" << endl;
-                accountNum();
+        for(int i=0;i<accountNumber.length();i++){ 
+      
+        if(!isalnum(accountNumber[i])){
+                alphaNum = false;
+                cout << "can only accept alphanumric values" << endl;
+                
+                break;
+        }                
+        else if (accountNumber.size() > ACCOUNT_LENGTH)
+        {
+                alphaNum = false;
+                cout << "Your account number must have 6 alphanumeric characters. Please enter again." << endl;
+                
+               break;
         }
-    
-
+        else if (accountNumber.size() < ACCOUNT_LENGTH)
+        {
+                alphaNum = false;
+                cout << "Your account number must have 6 alphanumeric characters. Please enter again." << endl;
+  
+               break;
+        }else{
+                alphaNum = true;
+                cout << "success" << endl;
+                break;
+        }
+        }
+        }while (accountNumber.size() != ACCOUNT_LENGTH);
+       }
+        
 }
 
-
-int main(){
+int main()
+{
         app();
         accountNum();
         cout << "next method call goes here" << endl;
         return 0;
 }
-
